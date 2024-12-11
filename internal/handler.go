a24a481a727a9f824d7d5af899c18b80024ef095
Shawgi Hafiz <shawgichan@gmail.com>
2024-12-11 11:49:01 +0400

    basic server

---
+++ internal/handler.go
@@ -0,0 +1,15 @@
+package internal
+
+import (
+	"net/http"
+)
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// handle
+}
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"UrlScan/internal"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	handler := internal.NewHandler()
+	mux.Handle("/scan", handler)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
